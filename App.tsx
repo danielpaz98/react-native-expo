76b4b2939784d6ed62ef0b939cd3fc0b0cc5ab5c
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-04-03 22:49:15 -0500

    feat: add FAB component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// COMPONENTS
+import FAB from './components/FAB';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+      
+      <FAB onPress={() => setCount(count + 1)} onLongPress={() => setCount(0)}>
+        +1
+      </FAB>
+
+      <FAB onPress={() => setCount(0)} position='left'>
+        Reset
+      </FAB>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100'
+  }
 });
---
+++ components/FAB.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Text, Pressable, StyleSheet, type StyleProp, type ViewStyle, type TextStyle } from 'react-native'
+
+interface Props extends React.ComponentProps<typeof Pressable> {
+	children?: React.ReactNode
+	style?: StyleProp<ViewStyle>;
+	textStyle?: StyleProp<TextStyle>;
+	position?: 'left' | 'right'
+}
+
+export default function FAB({ children, position = 'right', style, textStyle, ...restProps }: Props) {
+	return (
+		<Pressable style={({ pressed }) => [
+				styles.button, styles[position], pressed && styles.buttonPressed, style
+			]} 
+			{...restProps}
+		>
+			<Text style={[styles.text, textStyle]}>{children}</Text>
+		</Pressable>
+	)
+}
+
+const styles = StyleSheet.create({
+	button: {	
+    position: 'absolute',
+    bottom: 20,
+    backgroundColor: '#65658f',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0,height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+	buttonPressed: {
+		opacity: 0.7,
+	},
+	text: {
+		color: '#fff',
+		fontSize: 20,
+	},
+	right: { right: 20 },
+	left: { left: 20 },
+});
