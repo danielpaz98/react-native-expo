5d29fefa25282f0abc5b2d5d33228933496c9fa7
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-04-13 02:59:45 -0500

    feat: replace root layout with QueryClientProvider and Stack for routing

--- movies-app/src/app/_layout.tsx
+++ movies-app/src/app/_layout.tsx
@@ -1,11 +1,16 @@
-import { View, Text } from "react-native";
+// PLUGINS
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// EXPO
+import { Stack } from "expo-router";
 // GLOBAL STYLES
 import "@/global.css";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
-    <View>
-      <Text className="text-red-500">RootLayout</Text>
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </QueryClientProvider>
   );
 }
