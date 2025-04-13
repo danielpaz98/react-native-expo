378b6da90c25967c7e9cda31ef8cc1d9070db685
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-04-13 13:30:24 -0500

    feat: add PosterCard component for displaying movie posters

---
+++ movies-app/src/components/movies/PosterCard.tsx
@@ -0,0 +1,27 @@
+import { Pressable, Image } from "react-native";
+
+// MODELS
+import type { Movie } from "@/models/movie";
+// UTILS
+import { cn } from "@/utils";
+
+type MoviePoster = Pick<Movie, "id" | "poster">;
+
+interface Props extends Omit<React.ComponentPropsWithoutRef<typeof Pressable>, "id">, MoviePoster {
+  imageClassName?: string;
+  small?: boolean;
+}
+
+const baseClasses = "active:opacity-90 px-2";
+const baseImageClasses = "rounded-2xl shadow-lg w-[150px] h-[250px]";
+
+export default function PosterCard({ className, imageClassName, small = false, poster }: Props) {
+  const classNames = cn(baseClasses, className);
+  const imageClassNames = cn(baseImageClasses, { "w-[85px] h-[130px]": small }, imageClassName);
+
+  return (
+    <Pressable className={classNames}>
+      <Image className={imageClassNames} source={{ uri: poster }} resizeMode="cover" />
+    </Pressable>
+  );
+}
