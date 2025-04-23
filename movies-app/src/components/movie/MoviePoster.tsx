9e87673d1ca9363e60d244b48eb736ea68a28914
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-04-23 17:13:17 -0500

    refactor: convert MoviePoster to a forwardRef component for better ref handling

--- movies-app/src/components/movie/MoviePoster.tsx
+++ movies-app/src/components/movie/MoviePoster.tsx
@@ -1,3 +1,5 @@
+import { router } from "expo-router";
+import { forwardRef } from "react";
 import { Pressable, Image } from "react-native";
 
 // MODELS
@@ -15,13 +17,19 @@ interface Props extends Omit<React.ComponentPropsWithoutRef<typeof Pressable>, "
 const baseClasses = "active:opacity-90 px-2";
 const baseImageClasses = "rounded-2xl shadow-lg w-[150px] h-[250px]";
 
-export default function MoviePoster({ className, imageClassName, small = false, poster }: Props) {
-  const classNames = cn(baseClasses, className);
-  const imageClassNames = cn(baseImageClasses, { "w-[85px] h-[130px]": small }, imageClassName);
+const MoviePoster = forwardRef<React.ElementRef<typeof Pressable>, Props>(
+  ({ id, className, imageClassName, small = false, poster, ...restProps }, ref) => {
+    const classNames = cn(baseClasses, className);
+    const imageClassNames = cn(baseImageClasses, { "w-[85px] h-[130px]": small }, imageClassName);
 
-  return (
-    <Pressable className={classNames}>
-      <Image className={imageClassNames} source={{ uri: poster }} resizeMode="cover" />
-    </Pressable>
-  );
-}
+    return (
+      <Pressable ref={ref} className={classNames} {...restProps}>
+        <Image className={imageClassNames} source={{ uri: poster }} resizeMode="cover" />
+      </Pressable>
+    );
+  },
+);
+
+MoviePoster.displayName = "MoviePoster";
+
+export default MoviePoster;
