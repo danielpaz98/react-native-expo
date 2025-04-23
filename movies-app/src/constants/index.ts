4e7cec100e3fcbcd80b4e2e33f48924c03b9f687
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-04-23 18:11:35 -0500

    refactor: add STALE_TIME constant for cache management

--- movies-app/src/constants/index.ts
+++ movies-app/src/constants/index.ts
@@ -1,3 +1,6 @@
 // ENV VARS
 export const API_URL = process.env.EXPO_PUBLIC_TMDB_API_URL;
 export const API_KEY = process.env.EXPO_PUBLIC_TMDB_API_KEY;
+
+// CONSTANTS
+export const STALE_TIME = 1000 * 60 * 60 * 24; // 24 hours
