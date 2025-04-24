e57d052dbead4dda5b682dcde0fa1453316e7549
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-04-24 15:24:28 -0500

    feat: wrap RootLayout in GestureHandlerRootView for gesture handling support

--- movies-app/src/app/_layout.tsx
+++ movies-app/src/app/_layout.tsx
@@ -2,6 +2,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // EXPO
 import { Stack } from "expo-router";
+// NATIVE
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 // GLOBAL STYLES
 import "@/global.css";
 
@@ -9,8 +11,10 @@ const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{ headerTitle: "Movies App" }} />
-    </QueryClientProvider>
+    <GestureHandlerRootView>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerTitle: "Movies App" }} />
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }
