96890249d9c40c82460561a9c375526daff9d594
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-05-16 03:40:10 -0500

    feat(ThemeSwitch): add new component for theme switching functionality

---
+++ components-app/src/components/shared/ThemedSwitch.tsx
@@ -0,0 +1,50 @@
+import { Switch, Pressable, Platform, type SwitchProps } from "react-native";
+// UTILS
+import { cn } from "@/utils";
+// COMPONENTS
+import { ThemedText } from "@/components/shared";
+// VARIANTS
+import { textStyles, type TextVariants } from "@/components/shared/ThemedText";
+// HOOKS
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+interface Props {
+  value?: SwitchProps["value"];
+  text?: string;
+  className?: string;
+  textClassName?: string;
+  textVariant?: TextVariants["variant"];
+  textSize?: TextVariants["size"];
+  onValueChange?: SwitchProps["onValueChange"];
+}
+
+const isAndroid = Platform.OS === "android";
+
+export default function ThemedSwitch({
+  value,
+  text,
+  className,
+  textClassName,
+  textSize,
+  textVariant,
+  onValueChange,
+}: Props) {
+  const classNames = cn("flex flex-row items-center justify-end active:opacity-80", className);
+  const textClassNames = cn("flex-1", textStyles({ variant: textVariant, size: textSize }), textClassName);
+  const primaryColor = useThemeColor({}, "primary");
+
+  return (
+    <Pressable className={classNames} onPress={() => onValueChange?.(!value)}>
+      {text && <ThemedText className={textClassNames}>{text}</ThemedText>}
+      <Switch
+        value={value}
+        onValueChange={onValueChange}
+        thumbColor={isAndroid ? primaryColor : ""}
+        trackColor={{
+          false: "gray",
+          true: !isAndroid ? primaryColor : "",
+        }}
+      />
+    </Pressable>
+  );
+}
--- components-app/src/components/shared/index.ts
+++ components-app/src/components/shared/index.ts
@@ -1,4 +1,5 @@
-export { default as ThemedCard } from "./ThemedCard";
+export { default as ThemedSwitch } from "./ThemedSwitch";
 export { default as ThemedButton } from "./ThemedButton";
+export { default as ThemedCard } from "./ThemedCard";
 export { default as ThemedText } from "./ThemedText";
 export { default as ThemedView } from "./ThemedView";
