1454623354e08786b6e3920bf1d9416499dab8ef
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-05-16 03:35:31 -0500

    feat(ThemeCard): add new component for theming support

---
+++ components-app/src/components/shared/ThemedCard.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+// UTILS
+import { cn } from "@/utils";
+
+type Props = React.ComponentProps<typeof View>;
+
+export default function ThemedCard({ className, children, ...restProps }: Props) {
+  const classNames = cn("bg-white dark:bg-black/10 rounded-xl py-2 px-4 shadow-black/5", className);
+
+  return (
+    <View className={classNames} {...restProps}>
+      {children}
+    </View>
+  );
+}
--- components-app/src/components/shared/index.ts
+++ components-app/src/components/shared/index.ts
@@ -1,3 +1,4 @@
+export { default as ThemedCard } from "./ThemedCard";
 export { default as ThemedButton } from "./ThemedButton";
 export { default as ThemedText } from "./ThemedText";
 export { default as ThemedView } from "./ThemedView";
