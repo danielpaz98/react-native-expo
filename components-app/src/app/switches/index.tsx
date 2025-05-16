31a7b007ac139814845dc79ae0691532d9268b67
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-05-16 03:52:58 -0500

    feat(SwitchesScreen): implement themed switches for interactive state management

--- components-app/src/app/switches/index.tsx
+++ components-app/src/app/switches/index.tsx
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+// COMPONENTS
+import { ThemedView, ThemedCard, ThemedSwitch } from "@/components/shared";
 
 export default function SwitchesScreen() {
+  const [state, setState] = useState({ isActive: true, isHungry: false, isHappy: true });
+
   return (
-    <View>
-      <Text>SwitchesScreen</Text>
-    </View>
+    <ThemedView className="m-2.5 flex-1">
+      <ThemedCard>
+        <ThemedSwitch
+          text="Activo"
+          value={state.isActive}
+          onValueChange={(isActive) => setState({ ...state, isActive })}
+        />
+
+        <ThemedSwitch
+          text="Hambriento"
+          value={state.isHungry}
+          onValueChange={(isHungry) => setState({ ...state, isHungry })}
+        />
+
+        <ThemedSwitch
+          text="Feliz"
+          value={state.isHappy}
+          onValueChange={(isHappy) => setState({ ...state, isHappy })}
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 }
