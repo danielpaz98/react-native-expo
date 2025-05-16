fc7cc2133980b696d8ac1a5a3ba07caf6522c677
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-05-16 02:38:38 -0500

    feat(Animation102Screen): implement pan responder for draggable animation

--- components-app/src/app/animation-102/index.tsx
+++ components-app/src/app/animation-102/index.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from "react-native";
+import { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
+// COMPONENTS
+import { ThemedView } from "@/components/shared";
 
 export default function Animation102Screen() {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], { useNativeDriver: false }),
+    onPanResponderRelease: () => {
+      Animated.spring(pan, { toValue: { x: 0, y: 0 }, useNativeDriver: false }).start();
+    },
+  });
+
   return (
-    <View>
-      <Text>Animation102Screen</Text>
-    </View>
+    <ThemedView className="m-2.5 flex-1 items-center justify-center">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl size-36"
+        {...panResponder.panHandlers}
+        style={pan.getLayout()}
+      />
+    </ThemedView>
   );
 }
