1fd7116f3522abbda006ed2a541c49cd6104a0ac
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-05-16 02:25:14 -0500

    feat(Animation101Screen): implement useAnimation hook

--- components-app/src/app/animation-101/index.tsx
+++ components-app/src/app/animation-101/index.tsx
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { Animated, Easing } from "react-native";
+// COMPONENTS
+import { ThemedView, ThemedButton } from "@/components/shared";
+// HOOKS
+import { useAnimation } from "@/hooks/useAnimation";
 
 export default function Animation101Screen() {
+  const { animatedTop, animatedOpacity, fadeIn, fadeOut, slideInFromTop } = useAnimation();
+
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView className="m-2.5 flex-1 items-center justify-center gap-5">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl size-[150px]"
+        style={{ opacity: animatedOpacity, transform: [{ translateY: animatedTop }] }}
+      />
+
+      <ThemedButton
+        onPress={() => {
+          fadeIn();
+          slideInFromTop({ easing: Easing.bounce, duration: 700 });
+        }}
+      >
+        FadeIn
+      </ThemedButton>
+
+      <ThemedButton onPress={() => fadeOut()}>FadeOut</ThemedButton>
+    </ThemedView>
   );
 }
