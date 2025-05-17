2f8e91b64b018871993d952c22d2738c688034f6
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-05-17 14:46:27 -0500

    feat(SectionListScreen): implement section list with themed components and character data

--- components-app/src/app/section-list/index.tsx
+++ components-app/src/app/section-list/index.tsx
@@ -1,9 +1,39 @@
-import { View, Text } from "react-native";
+import { SectionList } from "react-native";
+// COMPONENTS
+import { ThemedView, ThemedCard, ThemedText } from "@/components/shared";
+// DATA
+import { houses } from "./data";
 
 export default function SectionListScreen() {
   return (
-    <View>
-      <Text>SectionListScreen</Text>
-    </View>
+    <ThemedView className="mx-2.5">
+      <ThemedCard className="">
+        <SectionList
+          keyExtractor={(item) => item}
+          sections={houses}
+          renderItem={({ item }) => <ThemedText>{item}</ThemedText>}
+          ListHeaderComponent={
+            <ThemedText size="h1" className="mb-3 font-bold">
+              Personajes
+            </ThemedText>
+          }
+          renderSectionHeader={({ section }) => (
+            <ThemedText size="h2" className="p-2 rounded bg-light-background dark:bg-dark-background">
+              {section.title}
+            </ThemedText>
+          )}
+          stickySectionHeadersEnabled
+          ListFooterComponent={
+            <ThemedText
+              size="h1"
+              className="p-2 mb-10 font-bold rounded bg-light-background dark:bg-dark-background"
+            >
+              Secciones: {houses.length}
+            </ThemedText>
+          }
+          showsVerticalScrollIndicator={false}
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 }
