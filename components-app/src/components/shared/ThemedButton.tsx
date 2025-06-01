9f527b9d6ba2082baa6e6853ac7cf257052c7617
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-06-01 04:27:58 -0500

    refactor(ThemedButton): enhance buttonVariants definition and update Props interface for improved type safety

--- components-app/src/components/shared/ThemedButton.tsx
+++ components-app/src/components/shared/ThemedButton.tsx
@@ -1,10 +1,27 @@
 import { Pressable } from "react-native";
+// PLUGINS
+import { cva, type VariantProps } from "class-variance-authority";
 // UTILS
 import { cn } from "@/utils";
 // COMPONENTS
 import ThemedText, { textStyles, type TextVariants } from "./ThemedText";
 
-interface Props extends React.ComponentProps<typeof Pressable> {
+const buttonVariants = /* tw */ cva(
+  "inline-flex items-center justify-center rounded-xl bg-light-primary dark:bg-dark-primary active:opacity-80",
+  {
+    variants: {
+      size: {
+        default: "px-6 py-2",
+        icon: "size-8 rounded-md",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  },
+);
+
+interface Props extends React.ComponentProps<typeof Pressable>, VariantProps<typeof buttonVariants> {
   children?: React.ReactNode;
   textClassName?: string;
   textVariant?: TextVariants["variant"];
@@ -14,20 +31,21 @@ interface Props extends React.ComponentProps<typeof Pressable> {
 export default function ThemedButton({
   children,
   className,
+  size,
   textClassName,
   textVariant,
   textSize,
   ...restProps
 }: Props) {
-  const classNames = cn(
-    "bg-light-primary items-center rounded-xl px-6 py-2 dark:bg-dark-primary active:opacity-80",
-    className,
-  );
-  const textClassNames = cn(textStyles({ variant: textVariant, size: textSize }), textClassName);
-
   return (
-    <Pressable className={classNames} {...restProps}>
-      <ThemedText className={textClassNames}>{children}</ThemedText>
+    <Pressable className={cn(buttonVariants({ size, className }))} {...restProps}>
+      <ThemedText
+        className={cn(textStyles({ variant: textVariant, size: textSize, className: textClassName }))}
+        variant={textVariant}
+        size={textSize}
+      >
+        {children}
+      </ThemedText>
     </Pressable>
   );
 }
