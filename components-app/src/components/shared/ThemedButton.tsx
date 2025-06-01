ba5f974d4caf98b2c4add9fc7156a40077980da9
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-06-01 04:09:30 -0500

    refactor(ThemedButton): update Props interface and adjust textStyles usage for clarity

--- components-app/src/components/shared/ThemedButton.tsx
+++ components-app/src/components/shared/ThemedButton.tsx
@@ -4,24 +4,26 @@ import { cn } from "@/utils";
 // COMPONENTS
 import ThemedText, { textStyles, type TextVariants } from "./ThemedText";
 
-interface Props extends React.ComponentProps<typeof Pressable>, TextVariants {
+interface Props extends React.ComponentProps<typeof Pressable> {
   children?: React.ReactNode;
   textClassName?: string;
+  textVariant?: TextVariants["variant"];
+  textSize?: TextVariants["size"];
 }
 
 export default function ThemedButton({
   children,
   className,
   textClassName,
-  variant,
-  size,
+  textVariant,
+  textSize,
   ...restProps
 }: Props) {
   const classNames = cn(
     "bg-light-primary items-center rounded-xl px-6 py-2 dark:bg-dark-primary active:opacity-80",
     className,
   );
-  const textClassNames = cn(textStyles({ variant, size }), textClassName);
+  const textClassNames = cn(textStyles({ variant: textVariant, size: textSize }), textClassName);
 
   return (
     <Pressable className={classNames} {...restProps}>
