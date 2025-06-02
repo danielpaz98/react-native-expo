f465999a17277aef6ae95d415532a634e4eabb82
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-06-02 01:24:57 -0500

    feat(infinite-scroll): implement InfiniteScrollScreen with dynamic loading of images

--- components-app/src/app/infinite-scroll/index.tsx
+++ components-app/src/app/infinite-scroll/index.tsx
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { FlatList } from "react-native";
+// COMPONENTS
+import { ThemedView } from "@/components/shared";
+import { FadeInImage } from "@/components";
 
 export default function InfiniteScrollScreen() {
+  const [numbers, setNumbers] = useState<number[]>([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const nextNumbers = Array.from({ length: 5 }, (_, i) => numbers.length + i);
+    setNumbers((prev) => [...prev, ...nextNumbers]);
+  };
+
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
+    <ThemedView className="px-5">
+      <FlatList
+        contentContainerClassName="gap-5"
+        data={numbers}
+        keyExtractor={(item) => item.toString()}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.6}
+        renderItem={({ item }) => (
+          <FadeInImage source={{ uri: `https://picsum.photos/800/700?random=${item}` }} />
+        )}
+      />
+    </ThemedView>
   );
 }
