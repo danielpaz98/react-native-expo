6eaaa3d286fc20f3d8a5786f6c550cf9ffe5f1ed
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2025-06-03 02:50:55 -0500

    feat(ThemesScreen): implement themed view with switches for dark and system modes

--- components-app/src/app/themes/index.tsx
+++ components-app/src/app/themes/index.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from "react-native";
+// COMPONENTS
+import { ThemedView, ThemedCard, ThemedSwitch } from "@/components/shared";
+// HOOKS
+import { useThemeChangerContext } from "@/contexts/ThemeChangerContext";
 
 export default function ThemesScreen() {
+  const { isDarkMode, isSystemTheme, toggleTheme, setSystemTheme } = useThemeChangerContext();
+
   return (
-    <View>
-      <Text>ThemesScreen</Text>
-    </View>
+    <ThemedView className="m-2.5">
+      <ThemedCard>
+        <ThemedSwitch
+          className="border-b-2 border-b-gray-200/50 dark:border-b-gray-800/50"
+          text="Dark Mode"
+          value={isDarkMode}
+          onValueChange={toggleTheme}
+          disabled={isSystemTheme}
+        />
+        <ThemedSwitch text="System Mode" value={isSystemTheme} onValueChange={setSystemTheme} />
+      </ThemedCard>
+    </ThemedView>
   );
 }
